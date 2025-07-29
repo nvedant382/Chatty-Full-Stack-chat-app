bf79dd12ab65a6ac7fa60e5840a0a6ad735070c7
nvedant382 <nvedant382@gmail.com>
2025-07-29 21:46:58 +0530

    fixed deplyment issues

--- server/src/index.js
+++ server/src/index.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")))
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     })
 }
