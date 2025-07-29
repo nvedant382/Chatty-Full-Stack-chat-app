5baa9ec1a639021176dad2dc4d1197ab236bc1a2
nvedant382 <nvedant382@gmail.com>
2025-07-29 21:42:05 +0530

    fixed deplyment issues

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -21,7 +21,7 @@ const userSocketMap = {}
 io.on("connection", (socket) => {
     console.log("A user connected: ", socket.id)
 
-    const userId = socket.handshake.query.userId
+    const userId = socket.handshake.auth.userId
     if (userId) userSocketMap[userId] = socket.id
 
     io.emit("getOnlineUsers", Object.keys(userSocketMap))
